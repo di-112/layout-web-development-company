501f5e9504fdb64c9d72d939b27a1a3fd6a0a13c
di-112 <73027626+di-112@users.noreply.github.com>
2021-03-01 21:00:06 +0300

    added transition

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-/*$(document).ready(function() {
-	$('.btn__mod').hover( function(event) {
-		$('.item__title,.item__text, .btn__mod, .grid__item, .imgPhone').toggleClass('hover');
-	});
-});*/
 const gridBtns = document.querySelectorAll('.btn__mod');
 
 if(gridBtns.length>0) for(let gridBtn of gridBtns) gridBtn.addEventListener('mouseover',  ()=> {
@@ -18,3 +13,5 @@ if(gridBtns.length>0) for(let gridBtn of gridBtns) gridBtn.addEventListener('mou
 		const hoverElements = document.querySelectorAll('.hover');
 		for(let hoverElement of hoverElements)hoverElement.classList.remove('hover');
 })
+
+
