57fb864665f5adaad04ad5d715a5cc3ea43aa593
di-112 <73027626+di-112@users.noreply.github.com>
2021-03-02 20:02:07 +0300

    burger menu

--- js/script_burger.js
+++ js/script_burger.js
@@ -3,4 +3,7 @@ $(document).ready(function() {
 		$('.header__burger,.menu__list').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+	$('.list__item').click( function(event) {
+		if(screen.width <= 725)$('.header__burger,.menu__list').removeClass('active');
+	});
 });
